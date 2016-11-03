65b01db89e30c6a1148026e9df47b0968ea3ab25
Brian Snyder <bsnyder@technology4retailers.com>
2016-11-03 10:11:27 -0500

    initial commit

---
+++ Farmer/Farmer/Farmer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmer
+{
+    class Farmer
+    {
+        public int BagsOfFeed;
+        public const int FeedMultiplier = 30;
+
+        private int numberOfCows;
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+            set
+            {
+                numberOfCows = value;
+                BagsOfFeed = numberOfCows * FeedMultiplier;
+            }
+        }
+
+
+    }
+}
